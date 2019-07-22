3e061dded87b3aa7bc2decab9f5d85cdf970d9b4
merlox <merunasgrincalaitis@gmail.com>
2019-07-22 19:08:23 +0200

    Redesigned app with object oriented programming for better maintenance

--- reto_uno.py
+++ reto_uno.py
@@ -17,21 +17,57 @@
 from base58 import b58encode
 from random import randint
 
-tickets = []
 estado = ('v', 'u')
-ticket_counter = 11111
 
-# Crea la cantidad especificada de tickets y los añade al array de tickets
-def crear_tickets(cantidad):
-    global ticket_counter
-    for i in range(cantidad):
-        ticket = {
-            "id_ticket": b58encode(str(randint(1e9, 9999999999))),
-            "id_evento": b58encode(str(ticket_counter)),
-            "numero_entrada": randint(1, 1e5),
-            "estado": estado[randint(0, 1)]
-        }
-        ticket_counter += 1
-        print("Id ticket {}, Id evento {}, Numero {}, Estado {}".format(ticket['id_ticket'], ticket['id_evento'], ticket['numero_entrada'], ticket['estado']))
+class Event_Manager:
+    events = []
+    ticket_counter = 11111 # Empieza en 11111 según los requisitos del programa
 
-crear_tickets(100)
+    # Crea la cantidad especificada de tickets y los añade al array de tickets
+    def create_ticket(self):
+        ticket = Ticket(
+            b58encode(str(randint(1e9, 9999999999))),
+            b58encode(str(self.ticket_counter)),
+            randint(1, 1e5),
+            estado[randint(0, 1)]
+        )
+        self.ticket_counter += 1
+        print("Id ticket {}, Id evento {}, Numero {}, Estado {}".format(ticket.id_ticket, ticket.id_evento, ticket.numero_entrada, ticket.estado))
+
+    # Crea la cantidad especificada de eventos y le añade tickets para cada uno entre 2 y 10 tickets
+    def create_event(self):
+        # Primero decidimos la cantidad de tickets a crear para el evento
+        tickets_to_create = randint(2, 10)
+        print('Creating event with {} tickets'.format(tickets_to_create))
+        my_tickets = []
+        for i in range(tickets_to_create):
+            my_tickets.append(self.create_ticket())
+
+        # Despues creamos el evento y añadimos los tickets
+        self.events.append(Event(my_tickets))
+
+class Event:
+    tickets = []
+
+    def __init__(self, tickets):
+        self.tickets = tickets
+
+class Ticket:
+    id_ticket = 0
+    id_evento = 0
+    numero_entrada = 0
+    estado = None
+
+    def __init__(self, id_ticket, id_evento, numero_entrada, estado):
+        self.id_ticket = id_ticket
+        self.id_evento = id_evento
+        self.numero_entrada = numero_entrada
+        self.estado = estado
+
+# Al comenzar, crear una instancia del event manager y crear 10 eventos
+def start():
+    event_manager = Event_Manager()
+    for i in range(10):
+        event_manager.create_event()
+
+start()
